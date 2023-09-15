2bb88f416ae284497adbf3d00de3b533427f9dca
Lucas <lucasgoncalves08@live.com>
2023-09-15 13:59:29 -0300

    finishing zx9-speaker_container styles for mobile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: [
     './home.component.css',
-
+    './styles/products-section.css'
   ]
 })
 export class HomeComponent {
