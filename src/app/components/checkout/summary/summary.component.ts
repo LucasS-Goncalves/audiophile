5decebcf9561dd9b46f05928681fb8e1dccc239c
Lucas <lucasgoncalves08@live.com>
2023-10-26 15:06:39 -0300

    working on summary info section

--- src/app/components/checkout/summary/summary.component.ts
+++ src/app/components/checkout/summary/summary.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+  styleUrls: [
+    './summary.component.css',
+    './styles/summaryProducts-section.css',
+    './styles/summaryInfo-section.css'
+  ]
 })
 export class SummaryComponent {
 
