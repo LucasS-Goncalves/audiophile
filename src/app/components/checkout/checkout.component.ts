28674e5ebe3ff7d7c343466d28467f0f872376ed
Lucas <lucasgoncalves08@live.com>
2023-11-21 19:12:28 -0300

    adding form validation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -8,25 +9,68 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
     './styles/form.css'
   ]
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit, AfterViewInit{
 
+  checkoutForm!: FormGroup;
   eMoney = true;
   cash = false;
   teste = false;
   @ViewChild('modal', {static: false}) modal!: ElementRef<HTMLDialogElement>;
 
+  ngOnInit(): void {
+    this.checkoutForm = new FormGroup({
+      'billing': new FormGroup({
+        'name': new FormControl(null, [Validators.required, Validators.pattern(/\S/)]),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'phone': new FormControl(null, [Validators.required, Validators.pattern(
+          /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
+          )])
+      }),
+      'shipping': new FormGroup({
+        'address': new FormControl(null, [Validators.required, Validators.pattern(/\S/)]),
+        'zipCode': new FormControl(null, [Validators.required, Validators.pattern(/^\d+$/)]),
+        'city': new FormControl(null, [Validators.required, Validators.pattern(/\S/)]),
+        'country': new FormControl(null, [Validators.required, Validators.pattern(/\S/)])
+      }),
+      'payment': new FormGroup({
+        'method': new FormControl("eMoney", Validators.required),
+        'eMoneyNumber': new FormControl(null, [Validators.required, Validators.pattern(/^\d+$/)]),
+        'eMoneyPin': new FormControl(null, [Validators.required, Validators.pattern(/^\d+$/)])
+      })
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.addFormValidators(['eMoneyNumber', 'eMoneyPin']);
+  }
+
   setPaymentMethodToEMoney(){
     this.cash = false;
     this.eMoney = true;
+    this.addFormValidators(['eMoneyNumber', 'eMoneyPin']);
   }
 
   setPaymentMethodToCash(){
     this.cash = true;
     this.eMoney = false;
+    (this.checkoutForm.get('payment') as FormGroup).get('eMoneyNumber')?.removeValidators(Validators.required);
+    (this.checkoutForm.get('payment') as FormGroup).get('eMoneyPin')?.removeValidators(Validators.required);
   }
 
   order(){
-    this.modal.nativeElement.showModal();
-    window.scroll(0, 0)
+    if(this.checkoutForm.valid){
+      //this.modal.nativeElement.showModal();
+      window.scroll(0, 0);
+    }
+  }
+
+  private addFormValidators(fieldList: string[] = []) {
+    fieldList.forEach(field => {
+        this.checkoutForm.get('payment')!.get(field)!.setValidators([Validators.required, Validators.pattern(/^\d+$/)]);
+    });
+}
+
+  onSubmit(){
+
   }
 }
